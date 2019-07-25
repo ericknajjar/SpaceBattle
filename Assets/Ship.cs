b7d0112d1366b2ef63fb18450f2bfefaf3cb855a
Erick <enajjar@gazeus.com.br>
2019-07-25 19:28:08 -0300

    first

---
+++ Assets/Ship.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    ShipManagger _shipManagger;
+
+    [SerializeField]
+    float _speed = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _shipManagger = FindObjectOfType<ShipManagger>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var okDistance = _speed * 1.5f * Time.deltaTime;
+        var ship = _shipManagger.FindClosestShip(this, okDistance);
+        if (ship != this)
+        {
+            var direction = (ship.Pos - Pos);
+
+            transform.Translate(direction.normalized * _speed * Time.deltaTime);
+        }
+    }
+
+
+    public Vector2 Pos
+    {
+        get
+        {
+            return transform.position;
+        }
+    }
+
+
+}
---
+++ Assets/ShipManagger.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipManagger : MonoBehaviour
+{
+    [SerializeField]
+    Ship _prefab;
+
+    [SerializeField]
+    int _numShips = 25;
+
+    // Start is called before the first frame update
+    List<Ship> _allShips;
+
+    void Awake()
+    {
+        _allShips = new List<Ship>(_numShips);
+        var min = Camera.main.ScreenToWorldPoint(Vector3.zero);
+        var max = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height));
+
+        for (int i = 0; i < _numShips; ++i)
+        {
+            var pos = RandomPos(min, max);
+            var go = Instantiate(_prefab.gameObject, pos, Quaternion.identity);
+            _allShips.Add(go.GetComponent<Ship>());
+        }
+    }
+
+    Vector2 RandomPos(Vector2 min, Vector2 max)
+    {
+        var x = UnityEngine.Random.Range(min.x, max.x);
+        var y = UnityEngine.Random.Range(min.y, max.y);
+
+        return new Vector2(x, y);
+    }
+
+    public Ship FindClosestShip(Ship ship, float okDistance)
+    {
+        var myPostion = ship.Pos;
+        var closest = ship;
+        float closestDistance = 999999;
+
+        foreach (var target in _allShips)
+        {
+            if (target == ship) continue;
+            var distance = Vector2.Distance(target.Pos, myPostion);
+
+            if (distance < closestDistance && distance > okDistance)
+            {
+                closestDistance = distance;
+                closest = target;
+            }
+        }
+
+        return closest;
+    }
+}
